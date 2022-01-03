b66405abca04973072257c14ea48dc2e2f7c013a
masta <mastanilewicz@gmail.com>
2022-01-03 22:50:38 +0000

    sign in and sign up pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ function Navbar() {
             <OfferIcon fill={pathMatchRoute('/offers') ? '#2c2c2c' : '#8f8f8f'} width='36px' height='36px'/>
             <p className={pathMatchRoute('/offers') ? 'navbarListItemNameActive' : 'navbarListItemName'}>Offer</p>
           </li>
-          <li className='navbarListItem' onClick={() => navigate('/profile')}>
+          <li className='navbarListItem' onClick={() => navigate('/sign-in')}>
             <PersonOutlineIcon fill={pathMatchRoute('/profile') ? '#2c2c2c' : '#8f8f8f'} width='36px' height='36px'/>
             <p className={pathMatchRoute('/profile') ? 'navbarListItemNameActive' : 'navbarListItemName'}>Profile</p>
           </li>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
+import {FaEye} from 'react-icons/fa'
 
 function SignUp() {
+  const [showPassword, setShowPassword] = useState(false)
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: ''
+  })
+
+  const {name, email, password} = formData
+
+  const navigate = useNavigate()
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }))
+  }
+
   return (
-    <div>
-      sign Up
-    </div>
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className='pageHeader'>
+            Register
+          </p>
+        </header>
+        <main>
+          <form>
+            <input onChange={onChange} type="text" className='nameInput' placeholder='Name' id='name' value={name}/>
+            <input onChange={onChange} type="email" className='emailInput' placeholder='Email' id='email' value={email}/>
+            <div className="passwordInputDiv">
+              <input onChange={onChange} type={showPassword ? 'text' : 'password'} className='passwordInput' placeholder='Password' id='password' value={password}/>
+              <FaEye onClick={() => setShowPassword((prev) => !prev)} className="showPassword" />
+            </div>
+            <Link to='/forgot-password' className='forgotPasswordLink'>Forgot Password?</Link>
+            <div className="signUpBar">
+              <p className="signUpText">
+                Sign Up
+              </p>
+              <button className='signUpButton'>
+                <ArrowRightIcon fill='#fff' width='34px' height='34px' />
+              </button>
+            </div>
+          </form>
+
+          {/* Google OAuth */}
+          <Link to='/sign-in' className='registerLink'>Sign In Instead</Link>
+        </main>
+      </div>
+    </>
   )
 }
 
