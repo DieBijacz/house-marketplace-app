93abb76312b940317ad2d6ff50bbc55e2d1c2a14
masta <mastanilewicz@gmail.com>
2022-01-08 12:12:26 +0000

    load images to firebase storage function

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,8 +1,11 @@
 import React, {useState, useEffect, useRef} from 'react'
 import {getAuth, onAuthStateChanged} from 'firebase/auth'
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import {db} from '../firebase.config'
 import {useNavigate} from 'react-router-dom'
 import Spinner from '../components/Spinner'
 import {toast} from 'react-toastify'
+import {v4 as uuidv4} from 'uuid'
 
 const apiKey = process.env.REACT_APP_API_KEY
 
@@ -42,6 +45,7 @@ function CreateListing() {
       toast.error('Discounted Price expected to be lower then Regular Price')
       return
     }
+
     if(imageUrls.length > 6){
       setLoading(false)
       toast.error('Max 6 images')
@@ -78,8 +82,59 @@ function CreateListing() {
       location = address
     }
 
-    setLoading(false)
+    // store image in firebase function
+    // DOCS https://firebase.google.com/docs/storage/web/upload-files?hl=en
+    const storeImage = async (image) => {
+      // when promise completed call resolve // if there was error call reject
+      return new Promise((resolve, reject) => {
+        const storage = getStorage()
+        // create a reference name
+        const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`
+        const storageRef = ref(storage, 'images/' + fileName)
 
+        // create upload task with created reference and passed image
+        const uploadTask = uploadBytesResumable(storageRef, image);
+
+        uploadTask.on('state_changed', 
+          (snapshot) => {
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+              case 'paused':
+                console.log('Upload is paused');
+                break;
+              case 'running':
+                console.log('Upload is running');
+                break;
+            }
+          }, 
+          (error) => {
+            reject(error)
+          }, 
+          () => {
+            // successful 
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+            });
+          }
+        );
+      })
+    }
+
+    const imgUrls = await Promise.all(
+      // loop through urls passed in form and call storeImage
+      [...imageUrls].map(image => storeImage(image))
+    ).catch(() => {
+      setLoading(false)
+      toast.error('Images not uploaded')
+      return
+    })
+
+    console.log(imgUrls);
+
+    setLoading(false)
   }
   
   const onMutate = (e) => {
